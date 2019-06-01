35dc809e4011febe389ca71bd9d0c6dd39b37428
JohnCoene <jcoenep@gmail.com>
2019-06-02 00:09:58 +0200

    init geo

---
+++ R/geo.R
@@ -0,0 +1,11 @@
+#' Geo
+#' 
+#' The Geographic API extends the Semantic API, using a linked data approach to enhance 
+#' location concepts used in The New York Times' controlled vocabulary and data resources 
+#' which combine them with the GeoNames database, an authoritative and free to use database 
+#' of global geographical places, names and features.
+#' 
+#' @export
+ny_geo <- function(q = NULL, name = NULL, longitude = NULL, latitude = NULL, elevation = NULL, filter = NULL, since = NULL, until = NULL, facets = NULL, sort = NULL){
+
+}
